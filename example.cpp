6c7d0ae974e84b25e1d7ae9b30ce0611abb5a3c6
admin <53293248+kvetinkavole@users.noreply.github.com>
2022-03-08 09:19:11 +0100

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 int main()
 {// 1 = true, 0 = false;
@@ -7,6 +8,12 @@ int main()
 	std::cout << "!= " << (x != 6) << std::endl; /*x does not equal 5*/
 	std::cout << "or " << (5 == x or x > 4) << std::endl; /*5 equals x or x is greater than 4*/
 	std::cout << "&& " << (x == 5 && x > 1) << std::endl; /*x is equal to 5 and at the same time x is greater than 1*/
+	std::cout << "round value: " << round(22.4) << std::endl; /*round number*/
+	std::cout << "max value: " << std::max(100, 80) << std::endl; /*finds the highest value and prints*/
+	std::cout << "min value: " << std::min(100, 80) << std::endl; /*here it finds the lowest value and prints*/
+
+	int a = std::max(100, 80); std::cout << a << std::endl; /*can be used in integer*/
+	a = std::min(100, 80); std::cout << a << std::endl;
 
 	if (x == 5, x != 6, 5 == x or x > 4, x == 5 && x > 1) /*if x equal to 5 and x does not equal 6 and x equal to 5 or x is greater than 4 and x equal to 5 &(and) x greater than 1 in same time -> console print Hello World!*/
 		std::cout << "\nHello World!\n"; /*print selected text*/	
